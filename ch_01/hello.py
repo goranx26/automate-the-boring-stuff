734e7a1e5cc903753e10b4f5a991226853eca10b
Goran Andersson <39906229+goranx26@users.noreply.github.com>
2026-04-19 20:27:07 +0200

    hello world added

---
+++ ch_01/hello.py
@@ -0,0 +1,10 @@
+# This program says hello and asks for my name.
+print('hello world')
+print('What is your name')
+my_name = input()
+print('It is good to meet you, ' + my_name)
+print('The length of your name is:')
+print(len(my_name))
+print('What is your age?')
+my_age = input()
+print('You will be ' + str(int(my_age) + 1) + ' in a year.')
